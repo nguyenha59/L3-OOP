2f0d23b9f2faf57afc01ba8373e52a8971a9293c
Nguyễn Hạ <166274048+nguyenha59@users.noreply.github.com>
2024-11-27 10:42:49 +0700

    Delete OtherProject directory

--- OtherProject/src/hust/soict/dsai/lab01/FirstDialog.java
+++
@@ -1,8 +0,0 @@
-package hust.soict.dsai.lab01;
-
-public class FirstDialog {
-    public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null,"Hello world! How are you?");
-        System.exit(0);
-    }
-}
